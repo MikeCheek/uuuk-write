545c656379170cca945bf660ea651a4110bec4bf
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2025-02-18 11:54:46 +0100

    Improved cursor

--- src/components/atoms/Cursor.tsx
+++ src/components/atoms/Cursor.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const Cursor: React.FC = () => {
+const Cursor = () => {
   const [smoothPosition, setSmoothPosition] = useState({ x: 0, y: 0 });
   const positionRef = useRef({ x: 0, y: 0 });
   const requestRef = useRef<number>();
@@ -36,27 +36,27 @@ const Cursor: React.FC = () => {
     <div className="pointer-events-none mix-blend-difference fixed top-0 left-0 z-50 w-full h-full isolate">
       {/* First Blob: Original white bordered blob */}
       <div
-        className="bg-secondary transition-all duration-100 ease-linear"
+        className="bg-secondary transition-all duration-[100ms] ease-linear"
         style={{
           position: "absolute",
-          width: "50px",
-          height: "50px",
-          transform: `translate(${smoothPosition.x - 25}px, ${smoothPosition.y - 25}px)`,
+          width: "40px",
+          height: "40px",
+          transform: `translate(${smoothPosition.x - 20}px, ${smoothPosition.y - 20}px)`,
           animation: "blob 10s infinite",
         }}
       />
 
-      {/* Second Blob: Rotated, slightly bigger with a different colour
+      {/* Second Blob: Rotated, slightly bigger with a different colour */}
       <div
-        className="bg-transparent border-2 border-blue transition-all duration-100 ease-linear"
+        className="bg-transparent border-2 border-secondary transition-all duration-[110ms] ease-linear opacity-80"
         style={{
           position: "absolute",
-          width: "60px",
-          height: "60px",
-          transform: `translate(${smoothPosition.x - 30}px, ${smoothPosition.y - 30}px) rotate(45deg)`,
-          animation: "blob 10s infinite reverse",
+          width: "50px",
+          height: "50px",
+          transform: `translate(${smoothPosition.x - 25}px, ${smoothPosition.y - 25}px) rotate(45deg)`,
+          animation: "blob 10s infinite",
         }}
-      /> */}
+      />
     </div>
   );
 };
