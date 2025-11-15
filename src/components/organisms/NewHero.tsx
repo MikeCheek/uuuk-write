0f14c10a8cfe35a49e4816fdb3c870de018d8a74
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2025-11-15 12:49:13 +0100

    Small button edit

--- src/components/organisms/NewHero.tsx
+++ src/components/organisms/NewHero.tsx
@@ -15,7 +15,7 @@ const NewHero = () => {
       <Logo className="absolute top-2 left-2 z-50"
         width={60} height={60} fill='#ecddbe' />
       <button onClick={toggleGallery} className="absolute z-50 top-4 right-4 px-4 py-2 border border-transparent text-lg font-medium rounded-lg text-darkBrown bg-beige focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-amber-700 focus:ring-beige transition duration-300 ease-in-out shadow-lg hover:shadow-xl transform hover:scale-105">
-        {galleryOpen ? "X" : "Open Gallery"}
+        {galleryOpen ? "X" : "Gallery"}
       </button>
       <Typography variant="h1" className="uppercase mb-4 md:mb-10 -mt-20 text-beige [text-shadow:_0_10px_10px_#ecddbe22] w-full text-center opacity-100">
         Write your story
