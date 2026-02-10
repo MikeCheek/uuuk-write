3d4632780deaa6e038687e5638ba6c2a3d0f6ace
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2026-02-10 17:11:37 +0100

    feat: add gatsby-plugin-google-tagmanager for enhanced tracking

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -103,6 +103,40 @@ const config: GatsbyConfig = {
           }
         }
       }
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-M9MR7CVS',
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: 'route-change',
+        // Defaults to false
+        enableWebVitalsTracking: true
+        // Defaults to https://www.googletagmanager.com
+        // selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+        // Defaults to gtm.js
+        // selfHostedPath: "YOUR_SELF_HOSTED_PATH",
+      }
     }
   ]
 }
