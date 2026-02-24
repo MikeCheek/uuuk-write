2075d17daca8fd81940366b5e81bd47e4ac99dd6
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2026-02-24 14:30:01 +0100

    fix: update Open Graph type and robots meta tag for improved SEO handling

--- src/components/atoms/Seo.tsx
+++ src/components/atoms/Seo.tsx
@@ -155,14 +155,14 @@ const Index = ({
       <meta property="og:url" content={seo.url} />
       <meta property="og:site_name" content={seo.title} />
       <meta property="og:description" content={description} />
-      <meta property="og:type" content={'website'} />
+      <meta property="og:type" content={price ? 'product' : 'website'} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={mainImage ?? metadata.siteUrl + fallbackImage?.src} />
 
-      <meta name="robots" content="max-image-preview:large" />
+      <meta name="robots" content="max-image-preview:standard" />
 
       {structuredData ? <Script type="application/ld+json">{JSON.stringify(microData)}</Script> : <></>}
       {noIndex ? <meta name="robots" content="noindex,nofollow" /> : <></>}
